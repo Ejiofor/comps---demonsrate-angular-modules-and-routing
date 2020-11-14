d534b5bc001065892db6630da53bcac2396d925f
Ejiofor <nwofor@ymail.com>
2020-11-14 20:02:21 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {ElementsModule} from './elements/elements.module';
+import {CollectionsModule} from './collections/collections.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ElementsModule,
+    CollectionsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
