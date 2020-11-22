d731dd6be0470371b316e76f27266194a1ca35c6
Ejiofor <nwofor@ymail.com>
2020-11-22 19:05:27 +0100

    ninth commit - table component

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    {name:'sara', city:'Hamburg', job: 'singer'},
+    {name:'Obi', city:'Munich', job: 'Footballer'},
+    {name:'Emea', city:'Bremen', job: 'Engineer'}
+  ];
+
+  headers = [
+    {key:'name', label:'name'},
+    {key:'city', label:'city'},
+    {key:'job', label:'job'}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
